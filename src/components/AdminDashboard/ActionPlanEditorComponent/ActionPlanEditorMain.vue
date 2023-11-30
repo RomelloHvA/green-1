943519ec368b4959740ea39ab46ceecbb59a1b11
Romello <romello.ten.broeke@hva.nl>
2023-11-30 18:14:59 +0100

    sectors are now also saved in the parent component and emitted in the from the SectorDropDownComponent.vue.

--- src/components/AdminDashboard/ActionPlanEditorComponent/ActionPlanEditorMain.vue
+++ src/components/AdminDashboard/ActionPlanEditorComponent/ActionPlanEditorMain.vue
@@ -1,6 +1,6 @@
 <template>
-Select a sector to edit those action plans:
-    <SectorDropDownComponent @sectorSelected="pushSelectedToRoute"/>
+  Select a sector to edit those action plans:
+  <SectorDropDownComponent @sectorSelected="pushSelectedToRoute" @sectors="setSectors"/>
   <SectorAllPlansComponent/>
 </template>
 
@@ -8,22 +8,21 @@ Select a sector to edit those action plans:
 import SectorAllPlansComponent from '@/components/AdminDashboard/ActionPlanEditorComponent/SectorAllPlansComponent'
 import router from '@/router'
 import SectorDropDownComponent from '@/components/adminquiz/SectorDropDownComponent'
-import { ref } from 'vue'
 
 export default {
   name: 'ActionPlanEditorMain',
   components: { SectorDropDownComponent, SectorAllPlansComponent },
   data () {
     return {
-      options: ['ICT', 'Fashion', 'Engineering'],
-      selectedOption: ref()
+      sectors: []
     }
   },
   methods: {
+    setSectors (sectors) {
+      this.sectors = sectors
+    },
     pushSelectedToRoute (selectedOption) {
-      if (selectedOption.name !== 'none' || selectedOption.name !== '') {
-        router.push(`/admin_dashboard/action_plans/${selectedOption.name}`)
-      }
+      router.push(`/admin_dashboard/action_plans/${selectedOption.name}`)
     }
   }
 }
