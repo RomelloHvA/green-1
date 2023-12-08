2e10a327993fca13c5a9da5437be98fab95ae089
Jiaming <chaosgamingnl33@gmail.com>
2023-12-08 19:58:41 +0100

    UpdateImage for imageController

--- green_1backend/src/main/java/app/rest/ImageController.java
+++ green_1backend/src/main/java/app/rest/ImageController.java
@@ -7,12 +7,10 @@ import app.repositories.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * RestController for finding images and updating image per page
@@ -27,12 +25,22 @@ public class ImageController {
     @Autowired
     private PageRepository pageRepository;
 
+    /**
+     * A method to retrieve all image objects
+     * @return all image objects in an array
+     */
     @GetMapping(path = "")
     public ResponseEntity<List<Image>> getAllImages() {
         List<Image> images = this.imageRepository.findAll();
         return ResponseEntity.ok(images);
     }
 
+    /**
+     * A method for getting an image from a certain page
+     * @param pageId = id of the page
+     * @return the Image that is found with the pageId
+     * @throws ResourceNotFoundException When there is no image found
+     */
     @GetMapping(path = "/{pageId}/all")
     public ResponseEntity<Image> getImageByPageId(@PathVariable(value = "pageId") Long pageId) throws ResourceNotFoundException {
         if (!pageRepository.existsById(Math.toIntExact(pageId))) {
@@ -40,5 +48,34 @@ public class ImageController {
         }
         Image pageImage = imageRepository.findByFkPageImage_PageId(pageId);
         return ResponseEntity.ok(pageImage);
+    }
+
+    /**
+     * A method for updating an image in the backend
+     * @param pageId = id of the page
+     * @param body = body of the request
+     * @return updated image
+     */
+    @PostMapping("/{pageId}/update")
+    public ResponseEntity<Image> updateImage(
+            @PathVariable(value = "pageId") Long pageId,
+            @RequestBody Map<String, String> body
+            ) {
+        String fileName = body.get("fileName");
+        String imageName = body.get("imageName");
+        Image image = null;
+        try {
+            image = this.imageRepository.findByFkPageImage_PageId(pageId);
+
+            if (image != null) {
+                image.setFileName(fileName);
+                image.setImageName(imageName);
+                return ResponseEntity.ok(image);
+            } else {
+                throw new ResourceNotFoundException("No image found with pageId = " + pageId);
+            }
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(image);
+        }
     }
 }
