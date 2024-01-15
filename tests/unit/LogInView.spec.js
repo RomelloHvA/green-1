22fd3dde809c44547ac8a0507e12ebe9e9174c31
Justin Chan <justin.chan2@hva.nl>
2024-01-15 15:38:51 +0100

    Succesfully failed login test

--- tests/unit/LogInView.spec.js
+++ tests/unit/LogInView.spec.js
@@ -8,11 +8,11 @@ import { enableFetchMocks } from 'jest-fetch-mock'
 enableFetchMocks()
 
 let wrapper
-
+const sessionService = shallowReactive(
+  new SessionSbService(CONFIG.BACKEND_URL + '/authentication/login'))
 beforeEach(async function () {
   fetch.resetMocks()
-  const sessionService = shallowReactive(
-    new SessionSbService(CONFIG.BACKEND_URL + '/authentication/login'))
+
   wrapper = mount(LogInView, {
     global: {
       plugins: [router],
@@ -56,27 +56,9 @@ describe('Trying to login', () => {
  */
 describe('Trying to login with invalid credentials', () => {
   it('cannot login with invalid credentials', async function () {
-    fetch.mockResponseOnce(null)
-
-    // Verifying whether the input field for username and password exists
-    const userNameField = wrapper.find('input[placeholder="Username"]')
-    const passwordField = wrapper.find('input[placeholder="Password"]')
-    const loginButton = wrapper.find('button[id="loginButton"]')
-    expect(userNameField.exists(),
-      'Cannot find input field with placeholder = Username').toBe(true)
-    expect(passwordField.exists(),
-      'Cannot find input field with placeholder = Password').toBe(true)
-    expect(loginButton.exists(),
-      'Cannot find button with id = loginButton').toBe(true)
-
-    // Attempt to log in with invalid user credentials
-    await userNameField.setValue('Kenny')
-    await passwordField.setValue('F@k3P@$$w0rd')
-    await loginButton.trigger('submit')
-
-    // Error message should exist when login attempt is unsuccessful
-    // expect(wrapper.vm.errorMessage).toBe('Invalid username or password')
-    expect(() => wrapper.vm.handleLogin().catch().toThrow('Invalid username or password'))
+    fetch.mockResponseOnce(mockJsonData, { status: 401 })
+    const response = await sessionService.asyncLogin(mockJsonData.username, mockJsonData.password)
+    expect(response).toEqual(false)
   })
 })
 
@@ -84,3 +66,10 @@ afterEach(function () {
   wrapper.unmount() // Clean up the wrapper after each test
   wrapper = null
 })
+
+const mockJsonData = {
+  id: 1,
+  username: 'Kenny',
+  password: 'F@k3P@$$w0rd',
+  isAdmin: false
+}
