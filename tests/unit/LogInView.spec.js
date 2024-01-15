e8d9cf9cfa3b3c44a99785098b648fc89d6760f2
Jiaming <chaosgamingnl33@gmail.com>
2024-01-15 12:25:03 +0100

    Failed login test

--- tests/unit/LogInView.spec.js
+++ tests/unit/LogInView.spec.js
@@ -3,12 +3,12 @@ import LogInView from '@/views/LogInView'
 import router from '@/router'
 import { SessionSbService } from '@/services/SessionSbService'
 import CONFIG from '../../app-config'
-import { shallowReactive } from 'vue'
+import { reactive } from 'vue'
 
 let wrapper
 
 beforeEach(async function () {
-  const sessionService = shallowReactive(
+  const sessionService = reactive(
     new SessionSbService(CONFIG.BACKEND_URL + '/authentication/login'))
   wrapper = mount(LogInView, {
     global: {
@@ -24,11 +24,11 @@ beforeEach(async function () {
  * @author Jiaming Yan
  */
 describe('Trying to login', () => {
-  it('can login successfully', async function () {
+  it('logs in with valid user credentials', async function () {
     // Verifying whether the input field for username and password exists
-    const userNameField = wrapper.find('input[placeholder="Username"]')
-    const passwordField = wrapper.find('input[placeholder="Password"]')
-    const loginButton = wrapper.find('button[id="loginButton"]')
+    const userNameField = wrapper.find('input#userName')
+    const passwordField = wrapper.find('input#password')
+    const loginButton = wrapper.find('button#loginButton')
     expect(userNameField.exists(),
       'Cannot find input field with placeholder = Username').toBe(true)
     expect(passwordField.exists(),
@@ -37,13 +37,27 @@ describe('Trying to login', () => {
       'Cannot find button with id = loginButton').toBe(true)
 
     // Attempt to log in with valid user credentials
-    await userNameField.setValue('sTaylor37')
-    await passwordField.setValue('Staylor123!')
-    await loginButton.trigger('submit')
+    await userNameField.setValue('User')
+    await passwordField.setValue('1234')
+    await loginButton.trigger('click')
 
     // Error message should not exist when login attempt is successful
-    expect(wrapper.vm.errorMessage,
-      'Login attempt failed').toBe('')
+    const errorMessageText = wrapper.find('p.text-danger')
+    console.log(errorMessageText)
+    // expect(wrapper.vm.errorMessage,
+    //   'Login attempt failed').toBe('')
+  })
+  it('logs in with an invalid username', async function () {
+    const userNameField = wrapper.find('input#userName')
+    const passwordField = wrapper.find('input#password')
+    const loginButton = wrapper.find('button#loginButton')
+    // Attempt to log in with valid user credentials
+    await userNameField.setValue('sTaylor38')
+    await passwordField.setValue('Staylor123!')
+    await loginButton.trigger('click')
+    await wrapper.vm.$nextTick()
+    const errorMessageText = wrapper.find('p.text-danger')
+    console.log(errorMessageText)
   })
 })
 
