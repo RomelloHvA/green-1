dea58ccb5494a3536d3960939ac5837680d06a0f
Jiaming <chaosgamingnl33@gmail.com>
2024-01-16 11:33:59 +0100

    GoalControllerTest

--- green_1backend/src/test/java/app/rest/GoalControllerTest.java
+++ green_1backend/src/test/java/app/rest/GoalControllerTest.java
@@ -1,54 +1,55 @@
 package app.rest;
 
+import app.models.Goal;
 import app.models.Quiz;
-import app.models.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UserControllerTest {
+public class GoalControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
-
-    private User user;
-    @Value("${server.servlet.context-path}")
-    private String servletContextPath;
+    private List<Goal> goals;
 
     @BeforeEach
     void setup() {
-        if (servletContextPath == null) {
-            servletContextPath = "/";
-        }
-        user = new User(0, "Alex95", "Alexisawesome95", false);
-    }
-    @Test
-    public void allUsersCanBeRetrieved() {
-        ResponseEntity<List<User>> response = restTemplate.exchange(
-                "/users/all",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<User>>() {}
+        this.goals = List.of(
+                new Goal(1, 1, "No Poverty"),
+                new Goal(2, 1, "Zero Hunger"),
+                new Goal(3, 1, "Good Health and Well-being")
         );
+    }
+
+    /**
+     * Testing if all Goals can be retrieved
+     * @author Jiaming Yan
+     */
+    @Test
+    public void allGoalsCanBeRetrieved() {
+
+        ResponseEntity<Goal[]> response =
+                this.restTemplate.getForEntity("/goals", Goal[].class);
 
         // check status code, location header and response body of post request
         assertEquals(HttpStatus.OK, response.getStatusCode(), "Status code should be 200 OK");
 
-        List<User> users = response.getBody();
-        assertThat(users.size(), is(greaterThan(0)));
+        Goal[] goalsList = response.getBody();
+        assertThat(goalsList.length, is(greaterThan(0)));
+        for (int i = 0; i < goalsList.length; i++) {
+            assertTrue(goalsList[i] instanceof Goal, "This is not a Goal object!");
+        }
     }
-
 }
